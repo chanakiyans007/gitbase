efe7848fb53e503dee1f1a8f850db77ee6f20679
chanakiyans007 <chanakiyans@gmail.com>
2025-03-01 15:14:54 +0530

    commit11

---
+++ src/test/java/practice/test/NewoneRaju.java
@@ -0,0 +1,11 @@
+package practice.test;
+
+public class NewoneRaju {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test is going");
+	}
+
+}
