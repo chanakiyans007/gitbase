a9301f9125d880a6c6947df047b699e17fc64636
chanakiyans007 <chanakiyans@gmail.com>
2025-03-01 17:54:34 +0530

    commit1

---
+++ src/test/java/gitnew/Newsister.java
@@ -0,0 +1,13 @@
+package gitnew;
+
+public class Newsister {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hii");
+		System.out.println("bye");
+	}
+
+}
