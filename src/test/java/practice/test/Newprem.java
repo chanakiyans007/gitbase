077731d5bdf83ea900389f471764b6a3b6d68b80
chanakiyans007 <chanakiyans@gmail.com>
2025-03-01 15:28:39 +0530

    commit22

---
+++ src/test/java/practice/test/Newprem.java
@@ -0,0 +1,12 @@
+package practice.test;
+
+public class Newprem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("testing is prem");
+	}
+
+}
