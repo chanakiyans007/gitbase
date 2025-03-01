ef5fed6ad4ed4c221a45fd22bca6644ea5cd3de0
chanakiyans007 <chanakiyans@gmail.com>
2025-03-01 18:01:47 +0530

    commit2

---
+++ src/test/java/gitnew/NewOneIsAdded.java
@@ -0,0 +1,11 @@
+package gitnew;
+
+public class NewOneIsAdded {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("New All Is New");
+	}
+
+}
